43e4229a77cb7f02dff4b9b18dc0624a0cb37179
lucarin91 <lucarin@protonmail.com>
2024-03-08 14:20:21 +0100

    add script to inflate assets

---
+++ installer/scripts/get-assets.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Arduino CLI version
+version="0.35.3"
+
+base_dir=$(realpath "$(dirname "$(realpath "$0")")"/../..)
+assets_dir="$base_dir/installer/assets"
+
+mkdir -p "$assets_dir"
+
+download_arduino_cli(){
+    echo "Downloading Arduino CLI v${version} binaries"
+
+
+    linux_folder="$assets_dir/linux"
+    mac_folder="$assets_dir/mac"
+    win_folder="$assets_dir/win"
+
+    # Create directories for each platform
+    mkdir -p "$linux_folder"
+    mkdir -p "$mac_folder"
+    mkdir -p "$win_folder"
+
+    # Define URLs for the binaries
+    linux_url="https://github.com/arduino/arduino-cli/releases/download/v${version}/arduino-cli_${version}_Linux_64bit.tar.gz"
+    mac_url="https://github.com/arduino/arduino-cli/releases/download/v${version}/arduino-cli_${version}_macOS_64bit.tar.gz"
+    windows_url="https://github.com/arduino/arduino-cli/releases/download/v${version}/arduino-cli_${version}_Windows_64bit.zip"
+
+    # Download and unpack each binary
+    curl -L $linux_url | tar xz -C "$linux_folder"
+    curl -L $mac_url | tar xz -C "$mac_folder"
+    curl -L $windows_url -o "$win_folder/arduino-cli.zip" && unzip -o "$win_folder/arduino-cli.zip" -d "$win_folder" && rm "$win_folder/arduino-cli.zip"
+
+    # cleanup
+    rm -f "$linux_folder/LICENSE.txt"
+    rm -f "$mac_folder/LICENSE.txt"
+    rm -f "$win_folder/LICENSE.txt"
+}
+
+compile_arduino_sketch(){
+    echo "Compiling Arduino sketch"
+
+    compile_script=$(realpath "$base_dir/scripts/compile.sh")
+    "$compile_script" && cp "$base_dir/sketches/BLE_Scratch/build/arduino.mbed_nano.nano33ble/BLE_Scratch.ino.bin" "$base_dir/installer/assets/BLE_Scratch.ino.bin"
+}
+
+download_arduino_cli
+compile_arduino_sketch
