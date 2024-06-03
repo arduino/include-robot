dd1fcac50eff3f49847f6b6530bc2e1029ab6f9f
Christian Sarnataro <csarnataro@users.noreply.github.com>
2024-06-03 10:01:43 +0200

    fix(installer): checking cores before upload (#79)
    Added a function to check if all supported boards have their corresponding 'core' (like in arduino-cli core install) installed

---
+++ installer/src/supported-boards.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        fqbn: 'arduino:mbed_nano:nano33ble',
+        platform: 'arduino:mbed_nano',
+    },
+    {
+        fqbn: 'arduino:mbed_nano:nanorp2040connect',
+        platform: 'arduino:mbed_nano',
+    },
+];
